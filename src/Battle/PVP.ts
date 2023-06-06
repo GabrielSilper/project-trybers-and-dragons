673ae7b190a5abaf0237cbac511c6e0b1206bb1d
GabrielSilper <gabrielsilper.main@gmail.com>
2023-06-06 19:12:16 -0400

    R10 - Classe PVP criada.

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,24 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  private _player2: Fighter;
+
+  constructor(player1: Fighter, player2: Fighter) {
+    super(player1);
+    this._player2 = player2;
+  }
+
+  fight(): number {
+    this.player.attack(this._player2);
+    if (this._player2.lifePoints > 0) {
+      this._player2.attack(this.player);
+    }
+    if (this.player.lifePoints > 0 && this._player2.lifePoints > 0) {
+      this.fight();
+    }
+    return super.fight();
+  }
+}
+
+export default PVP;
