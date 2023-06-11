bbc7411a790fce9f7a7226fddba5762c594e09e4
GabrielSilper <gabrielsilper.main@gmail.com>
2023-06-11 14:06:00 -0400

    R13 - Objetos criados.

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Monster from './Monster';
+
+const player1 = new Character('Lautaro');
+const player2 = new Character('Haaland');
+const player3 = new Character('Raspadori');
+
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+
+const monster1 = new Monster();
+const monster2 = new Monster();
+
+const pvp = new PVP(player2, player3);
+const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles(battles: Battle[]) {
+  battles.forEach((battle) => battle.fight());
+}
+
+export { player1, player2, player3, monster1, monster2, pvp, pve, runBattles };
